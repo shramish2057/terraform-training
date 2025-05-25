fca2e94df73c430c22ad6063848039e3ef67c535
Shramish KSB <shramish.kafle@ksb.com>
2025-05-25 08:45:44 +0200

    fix. fix conflicting route table issue

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -55,17 +55,18 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_route_table" "private" {
+  count  = length(var.availability_zones)
   vpc_id = aws_vpc.main.id
 
   tags = merge(var.tags, {
-    Name = "${var.environment}-private-rt"
+    Name = "${var.environment}-private-rt-${count.index + 1}"
   })
 }
 
 resource "aws_route_table_association" "private" {
   count          = length(var.availability_zones)
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private[count.index].id
 }
 
 resource "aws_vpc_endpoint" "s3" {
@@ -105,7 +106,7 @@ resource "aws_nat_gateway" "nat" {
 
 resource "aws_route" "private_internet_access" {
   count                  = length(var.availability_zones)
-  route_table_id         = aws_route_table.private.id
+  route_table_id         = aws_route_table.private[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat[count.index].id
 }
