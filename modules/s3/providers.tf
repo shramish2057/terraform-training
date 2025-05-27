26a24986150eaac95877d03c821543411dbcd865
Shramish KSB <shramish.kafle@ksb.com>
2025-05-27 16:25:50 +0200

    fix region issue

--- modules/s3/providers.tf
+++ modules/s3/providers.tf
@@ -1,13 +1,4 @@
 provider "aws" {
   region = "us-east-1" # Hardcoding temporarily for debugging
   alias  = "s3"
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
 } 
